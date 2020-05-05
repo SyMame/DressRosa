3317fb9f64ce030a92c990707ed7a00db5a299ce
SyMame <sy.mame.diack@gmail.com>
2020-05-05 16:41:36 +0200

    Gestiondemotdepasseoublieetmajfichiers

--- MyApplication/app/src/main/java/com/example/myapplication/ResetPasswordFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ResetPasswordFragment.java
@@ -1,12 +1,38 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
+import android.icu.text.BidiRun;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
@@ -27,7 +53,16 @@ public class ResetPasswordFragment extends Fragment {
     public ResetPasswordFragment() {
         // Required empty public constructor
     }
+    private EditText registeredEmail;
+    private Button resetPasswordBtn;
+    private TextView goBack;
+    private ViewGroup emailIconContainer;
+    private ImageView emailIcon;
+    private TextView emailIconText;
+    private ProgressBar progressBar;
+    private FrameLayout parentFramelayout;
 
+    private FirebaseAuth firebaseAuth;
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -59,6 +94,135 @@ public class ResetPasswordFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reset_password, container, false);
+        View view = inflater.inflate(R.layout.fragment_reset_password, container, false);
+
+        registeredEmail = view.findViewById(R.id.forgot_password_email);
+        resetPasswordBtn = view.findViewById(R.id.reset_password_btn);
+        goBack = view.findViewById(R.id.forgot_password_go_back);
+        emailIconContainer = view.findViewById(R.id.forgot_password_email_icon_container);
+        emailIcon = view.findViewById(R.id.forgot_password_email_icon);
+        emailIconText = view.findViewById(R.id.forgot_password_email_icon_text);
+        progressBar = view.findViewById(R.id.forgot_password_progressbar);
+
+        parentFramelayout = getActivity().findViewById(R.id.register_framelayout);
+        firebaseAuth = firebaseAuth.getInstance();
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+        registeredEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(new SignInFragment());
+
+            }
+        });
+
+        resetPasswordBtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(emailIconContainer);
+                emailIconText.setVisibility(View.GONE);
+
+
+                TransitionManager.beginDelayedTransition(emailIconContainer);
+                emailIcon.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+
+                resetPasswordBtn.setEnabled(false);
+                resetPasswordBtn.setTextColor(Color.argb(50, 255, 255, 255));
+
+                firebaseAuth.sendPasswordResetEmail(registeredEmail.getText().toString())
+
+                        .addOnCompleteListener(new OnCompleteListener <Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    ScaleAnimation scaleAnimation = new ScaleAnimation(1,0,1,0,emailIcon.getWidth()/2,emailIcon.getHeight()/2);
+                                    scaleAnimation.setDuration(100);
+                                    scaleAnimation.setInterpolator(new AccelerateInterpolator());
+                                    scaleAnimation.setRepeatMode(Animation.REVERSE);
+                                    scaleAnimation.setRepeatCount(1);
+                                    scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+                                        @Override
+                                        public void onAnimationStart(Animation animation) {
+                                        }
+
+                                        @Override
+                                        public void onAnimationEnd(Animation animation) {
+                                            emailIconText.setText("Recovery Email Sent Successfully! Please Check your Inbox");
+                                            emailIconText.setTextColor(getResources().getColor(R.color.successGreen));
+                                            TransitionManager.beginDelayedTransition(emailIconContainer);
+                                            emailIcon.setVisibility(View.VISIBLE);
+                                            emailIconText.setVisibility(View.VISIBLE);
+                                        }
+
+                                        @Override
+                                        public void onAnimationRepeat(Animation animation) {
+                                            emailIcon.setImageResource(R.mipmap.green_mail);
+                                        }
+                                    });
+
+                                    emailIcon.startAnimation(scaleAnimation);
+
+                                    //Toast.makeText(getActivity(), "Email Sent Successfully", Toast.LENGTH_LONG).show();
+                                } else {
+                                    String error = task.getException().getMessage();
+                                    resetPasswordBtn.setEnabled(true);
+                                    resetPasswordBtn.setTextColor(Color.rgb(255, 255, 255));
+                                    emailIcon.setImageResource(R.mipmap.red_mail);
+                                    emailIconText.setText(error);
+                                    emailIconText.setTextColor(getResources().getColor(R.color.btnRed));
+                                    TransitionManager.beginDelayedTransition(emailIconContainer);
+                                    emailIconText.setVisibility(View.VISIBLE);
+                                    // Toast.makeText(getActivity(), error, Toast.LENGTH_LONG).show();
+                                }
+                                progressBar.setVisibility(View.GONE);
+
+                            }
+                        });
+            }
+        });
+    }
+
+    private void checkInputs() {
+        if (TextUtils.isEmpty(registeredEmail.getText())) {
+            resetPasswordBtn.setEnabled(false);
+            resetPasswordBtn.setTextColor(Color.argb(50, 255, 255, 255));
+        } else {
+            resetPasswordBtn.setEnabled(true);
+            resetPasswordBtn.setTextColor(Color.rgb(255, 255, 255));
+        }
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slideout_from_left, R.anim.slideout_from_right);
+        fragmentTransaction.replace(parentFramelayout.getId(), fragment);
+        fragmentTransaction.commit();
+    }
+
 }
+
+
